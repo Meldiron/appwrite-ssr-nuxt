9c4c6398bf2909ae7c2dcb4f4c112e746394a04f
Matej Bačo <matejbaco2000@gmail.com>
2023-04-19 19:27:34 +0200

    Copy layout over

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,54 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    "~/assets/main.css",
+    "~/node_modules/@appwrite.io/pink/dist/pink.css",
+    "~/node_modules/@appwrite.io/pink-icons/dist/icon.css",
+  ],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Almost SSR | Nuxt",
+      link: [
+        {
+          rel: "icon",
+          href: "/favicon.png",
+        },
+      ],
+      meta: [
+        {
+          name: "description",
+          content:
+            "Appwrite Loves Nuxt! Demo application with authorized server-side and client-side rendering.",
+        },
+        { property: "og:url", content: "https://nuxt.ssr.almostapps.eu/" },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: "Almost SSR | Nuxt" },
+        {
+          property: "og:description",
+          content:
+            "Appwrite Loves Nuxt! Demo application with authorized server-side and client-side rendering.",
+        },
+        {
+          property: "og:image",
+          content: "https://nuxt.ssr.almostapps.eu/cover.png",
+        },
 
-})
+        { property: "twitter:card", content: "summary_large_image" },
+        { property: "twitter:domain", content: "nuxt.ssr.almostapps.eu" },
+        { property: "twitter:url", content: "https://nuxt.ssr.almostapps.eu/" },
+        { property: "twitter:title", content: "Almost SSR | Nuxt" },
+        {
+          property: "twitter:description",
+          content:
+            "Appwrite Loves Nuxt! Demo application with authorized server-side and client-side rendering.",
+        },
+        {
+          property: "twitter:image",
+          content: "https://nuxt.ssr.almostapps.eu/cover.png",
+        },
+      ],
+    },
+  },
+});
